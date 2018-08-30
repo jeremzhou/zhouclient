a50751d5b692d2892e8430ed295f5c1ecd485f36
jeremy <Jeremy.zhou@utstarcom.cn>
2018-08-30 16:24:04 +0800

    first commit

---
+++ src/main/java/com/ctrip/framework/apollo/enums/ResourceType.java
@@ -0,0 +1,14 @@
+/**
+ * created on 2018年8月23日 下午4:22:32
+ */
+package com.ctrip.framework.apollo.enums;
+
+/**
+ * @author UTSC0167
+ * @date 2018年8月23日
+ *
+ */
+public enum ResourceType {
+
+    RESOURCE, CONFIG
+}
---
+++ src/main/java/com/ctrip/framework/apollo/util/ConfigConstants.java
@@ -0,0 +1,30 @@
+/**
+ * created on 2018年8月23日 下午6:14:44
+ */
+package com.ctrip.framework.apollo.util;
+
+/**
+ * @author UTSC0167
+ * @date 2018年8月23日
+ *
+ */
+public final class ConfigConstants {
+
+    private ConfigConstants() {
+        
+    }
+    
+    public static final String APP_ID = "app.id";
+    
+    public static final String ENV = "ENV";
+    
+    public static final String DEV_META = "dev_meta";
+    
+    public static final String UAPOLLO_CLIENT_IP = "uapollo.client.ip";
+    
+    public static final String UAPOLLO_ENV = "uapollo.env";
+    
+    public static final String UAPOLLO_SEVER_PORT = "uapollo.server-port";
+    
+    public static final String SPRING_APPLICATION_NAME = "spring.application.name";
+}
